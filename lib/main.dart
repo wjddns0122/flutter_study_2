a99e586fbbcb6e9cca39f1a514cf3d600f343c67
wjddns0122 <jwoon2004@gmail.com>
2023-05-06 11:33:57 +0900

    3complete

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:toonflix2/screens/home_screen.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          background: Color(0xFFE7626C),
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
+          ),
+        ),
+        cardColor: const Color(0xFFF4EDDB),
+      ),
+      home: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: const HomeScreen(),
+      ),
+    );
+  }
+}
